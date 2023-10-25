d8a8d4343375e34df121a30e709c51c424d347e8
Jemma Nelson <nelsonjs@altiusinstitute.org>
2023-10-25 11:40:16 -0700

    altcode alpha4 - summarize by library

--- processes/altcode/bin/summarize_stats.py
+++ processes/altcode/bin/summarize_stats.py
@@ -67,6 +67,64 @@ def revcom(bc):
         return None
     return "".join(REVCOM[x] for x in reversed(bc))
 
+def summarize_by_library(pool_info, stats):
+    """
+    Stats is a list of observed cell barcodes & how many we saw / how well they
+    mapped.
+    Given the stats and pool_info, sum/group them appropriately by barcode
+    Right now it's kind of backwards.
+    For example, a library with barcode2 = "TTTAAGCG" will contain all cells
+    that end with "_CGCTTAAA" (the reverse complement)
+    """
+    def build_barcode_to_library_lookup(pool_info, stats):
+        barcode_to_library = {}
+        for lib in pool_info["libraries"]:
+            bc = revcom(lib["barcode2"])
+            barcode_to_library[bc] = lib["library"]
+        return barcode_to_library
+
+
+    # Stub out keys
+    data = {
+        "barcode_mapping": {},
+        "barcode_stats": {},
+        "flowcell_label": {},
+        "pool": {},
+        "libraries": {},
+        "summary_stats": {},
+    }
+
+    bc_to_library = build_barcode_to_library_lookup(pool_info, stats)
+    libraries = {}
+    for cell in stats:
+        total_bc = cell["CB"]
+        if "_" not in total_bc:
+            if total_bc not in ["CBnotInPasslist"]:
+                logging.warning("Skipping possible barcode %s", total_bc)
+            continue
+        (_, _1, bc) = total_bc.split("_")
+        if bc not in libraries:
+            libraries[bc] = defaultdict(int)
+        for (k, v) in cell.items():
+            if k == "CB":
+                continue
+            libraries[bc][k] += int(v)
+    # Convert back to strings (ew)
+    for bc in libraries:
+        for (k, v) in libraries[bc].items():
+            libraries[bc][k] = str(v)
+
+    pool_set = set(lib["pool"] for lib in pool_info["libraries"])
+    assert len(pool_set) == 1, "Should have exactly 1 pool, instead: %s" % pool_set
+    data["pool"] = pool_set.pop()
+    flowcell_set = set(lib["flowcell"] for lib in pool_info["libraries"])
+    assert len(flowcell_set) == 1, "Pool should have exactly 1 flowcell, instead %s" % flowcell_set
+    data["flowcell_label"] = flowcell_set.pop()[2:]
+
+    data["barcode_mapping"] = bc_to_library
+    data["libraries"] = libraries
+
+    return data
 
 def summarize_by_sample(pool_info, stats):
     """
@@ -136,7 +194,7 @@ def main():
 
     samples = parse_cellreads(cell_reads_filename)
 
-    data = summarize_by_sample(cfg, samples)
+    data = summarize_by_library(cfg, samples)
     data["summary_stats"] = parse_summary_stats(os.path.join(gene_dir, "Summary.csv"))
     data["barcode_stats"] = parse_barcode_stats(os.path.join(opts.solo_dir, "Barcodes.stats"))
 
